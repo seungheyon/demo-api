813c8975766b6ff04b1e7f829d568dce8c1c698c
LSH <fhzktm@naver.com>
2024-01-17 00:42:32 +0900

    Post get, update api 테스트 작성

--- src/test/java/com/example/demoapi/post/service/PostServiceTest.java
+++ src/test/java/com/example/demoapi/post/service/PostServiceTest.java
@@ -2,6 +2,7 @@ package com.example.demoapi.post.service;
 
 import com.example.demoapi.DemoApiApplication;
 import com.example.demoapi.post.dto.PostRequestDto;
+import com.example.demoapi.post.dto.PostUpdateDto;
 import com.example.demoapi.post.entity.Post;
 import com.example.demoapi.post.repository.PostRepository;
 import com.example.demoapi.post.service.PostService;
@@ -10,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -24,22 +26,64 @@ public class PostServiceTest {
         this.postRepository = postRepository;
     }
 
-    @DisplayName("글 제목과 내용이 주어지면 테이블에 글이 추가된다.")
+    @DisplayName("sut는 postRequestDto 정보로 테이블에 post 를 생성한다.")
     @Test
     void createPostTest(){
         // Arrange
-        PostService sut = new PostService(postRepository);
-        String testTitle = "testTitle";
-        String testContents = "testContents";
-        var testDto = new PostRequestDto(testTitle, testContents);
+        var sut = new PostService(postRepository);
+        String title = "testTitle";
+        String contents = "testContents";
+        var testDto = new PostRequestDto(title, contents);
 
         // Act
         Post actual = sut.createPost(testDto);
 
         // Assert
         assertThat(actual.getId()).isNotNull();
-        assertThat(actual.getTitle()).isEqualTo(testTitle);
-        assertThat(actual.getContents()).isEqualTo(testContents);
+        assertThat(actual.getTitle()).isEqualTo(title);
+        assertThat(actual.getContents()).isEqualTo(contents);
+    }
+
+    @DisplayName("sut는 postId에 해당하는 post의 정보로 구성된 postResponseDto 를 반환한다.")
+    @Test
+    void getPostTest(){
+        // Arrange
+        var sut = new PostService(postRepository);
+        String title = "testTitle";
+        String contents = "testContents";
+
+        var createdPost = postRepository.save(new Post(null, title, contents));
+
+        // Act
+        var actual = sut.getPost(createdPost.getId());
+
+        // Assert
+        assertThat(actual.getTitle()).isEqualTo(title);
+        assertThat(actual.getContents()).isEqualTo(contents);
+    }
+
+    @DisplayName("sut는 postId에 해당하는 post의 정보를 postUpadateDto 정보로 수정한다.")
+    @Test
+    @Transactional
+    void updatePostTest(){
+        // Arrange
+        var sut = new PostService(postRepository);
+        String title = "title";
+        String contents = "contents";
+
+        var createdPost = postRepository.save(new Post(null, title, contents));
+
+        String newTitle = "newTitle";
+        String newContents = "newContents";
+        var testDto = new PostUpdateDto(newTitle, newContents);
+
+        // Act
+        sut.updatePost(createdPost.getId(), testDto);
+        var actual = postRepository.findById(createdPost.getId()).get();
+
+        // Assert
+        assertThat(actual.getTitle()).isEqualTo(newTitle);
+        assertThat(actual.getContents()).isEqualTo(newContents);
     }
 
 }
